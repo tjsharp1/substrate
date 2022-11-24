41881d09cfd425e2eebfaa03fadb6528756429c8
TJ Sharp <tjsharp@TJs-MacBook-Pro-2.local>
2022-11-24 14:45:14 -0500

    branch frum

--- primitives/runtime/src/generic/unchecked_extrinsic.rs
+++ primitives/runtime/src/generic/unchecked_extrinsic.rs
@@ -147,6 +147,7 @@ where
 	type Checked = CheckedExtrinsic<AccountId, Call, Extra>;
 
 	fn check(self, lookup: &Lookup) -> Result<Self::Checked, TransactionValidityError> {
+    log::error!("TJDEBUG sig here!");
 		Ok(match self.signature {
 			Some((signed, signature, extra)) => {
 				let signed = lookup.lookup(signed)?;
